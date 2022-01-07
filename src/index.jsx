2a59b329406c807ee2dfb56c12edbe608158316f
Renan Souza <renantrsouza@gmail.com>
2022-01-06 20:10:33 -0400

    create reset

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Reset } from './styles/generic/reset';
+import { Base } from './styles/elements/Base';
 import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
+    <Reset />
+    <Base />
     <App />
   </React.StrictMode>,
   document.getElementById('root')
---
+++ src/styles/elements/Base.js
@@ -0,0 +1,7 @@
+import {createGlobalStyle} from 'styled-components';
+
+export const Base = createGlobalStyle`
+    body{
+        font-family: 'Comfortaa', cursive;
+    }
+`;

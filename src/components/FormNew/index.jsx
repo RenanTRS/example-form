3b3d69e808f664a8bf462ac9240772c613fde9c4
Renan Souza <renantrsouza@gmail.com>
2022-01-12 11:46:37 -0400

    add regra yup para cpf

--- src/components/FormNew/index.jsx
+++ src/components/FormNew/index.jsx
@@ -3,19 +3,17 @@ import React from 'react';
 import { yupResolver } from '@hookform/resolvers/yup';
 import * as yup from 'yup'
 
-//import { schema } from './validation';
 import { useForm } from 'react-hook-form';
 
 import { Form } from '../Form';
 import { Field } from '../Field';
 import { Button } from '../Button';
 
-
 export const FormNew = () => {
-    //import * as yup from 'yup';
     const schema = yup.object({
         name: yup.string().min(2, 'Mínimo dois caracteres').required('Digite um nome'),
-        email: yup.string().email('Digite um email válido').required('Digite um email')
+        email: yup.string().email('Digite um email válido').required('Digite um email'),
+        cpf: yup.string().required('Digite um cpf')
     }).required();
     const { register, handleSubmit, formState:{ errors } } = useForm({
         resolver: yupResolver(schema)
@@ -29,6 +27,7 @@ export const FormNew = () => {
         <Form onSubmit={handleSubmit(newUser)}>
             <Field.Text label="Nome" type="text" register={register("name")} error={errors.name?.message}/>
             <Field.Text label="Email" type="email" register={register("email")} error={errors.email?.message} />
+            <Field.Cpf label="CPF" register={register("cpf")} error={errors.cpf?.message}/>
 
             <Button>Enviar</Button>
         </Form>

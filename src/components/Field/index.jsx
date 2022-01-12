8a43cf4847197c3825afd85212d01a4713866ae0
Renan Souza <renantrsouza@gmail.com>
2022-01-12 11:46:10 -0400

    Add input com máscara e validação customizada

--- src/components/Field/index.jsx
+++ src/components/Field/index.jsx
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, {useState} from 'react';
+import InputMask from 'react-input-mask';
 
 import { Label } from './Label';
 import { Content } from './Content';
-import { Input } from './Input';
+import { Input, InputM } from './Input';
 import { Error } from './Error';
+
+import { validation } from './Input/services/validation';
 
 const Text = ({label, type, name, register, error}) => {
     return (
@@ -15,6 +18,26 @@ const Text = ({label, type, name, register, error}) => {
     );
 }
 
+const Cpf = ({label, register, error}) => {
+    const [message, setMessage] = useState('');
+
+    function handleValidation(event){
+        setMessage(validation(event));
+    }
+
+    return (
+        <Label>
+            <Content>{label}</Content>
+            <InputM>
+                <InputMask mask="999.999.999-99" {...register} onInvalid={handleValidation} onBlur={handleValidation} />
+            </InputM>
+            {message && <Error>{message}</Error>}
+            {error && <Error>{error}</Error>}
+        </Label>
+    );
+}
+
 export const Field = {
-    Text
+    Text,
+    Cpf
 }
